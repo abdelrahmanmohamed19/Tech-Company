d50a2bf5bfc853de3cbc668a52cdbaed402ca196
Abdelrahman <abdelrahmanmohamed19912@gmail.com>
2023-05-31 12:29:04 +0300

    first commit

---
+++ JS/script.js
@@ -0,0 +1,11 @@
+document.getElementById("p1").innerHTML = "The success of technologies and e-marketing solutions depends on continuous management and support, and we in our company make you own all e-marketing solutions such as website design, e-store and phone applications with continuous support all the time. the company has been building many marketing solutions for all companies and commercial, industrial and service sectors by a team of specialists in providing and supporting e-marketing solutions.";
+
+
+function mOver(obj) {
+    obj.innerHTML = "Thank You"
+  }
+  
+  function mOut(obj) {
+    obj.innerHTML = "Get started"
+  }
+
